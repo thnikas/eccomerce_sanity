e9dbe94e1765db9b6505dc5882e04a242068024e
thnikas <8nikas@gmail.com>
2023-09-01 12:58:30 +0200

    <update 0.1>

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,12 +3,15 @@ import Head from 'next/head';
 
 import Navbar from './Navbar';
 import Footer from './Footer';
-
+import icon from '../public/favicon.ico'
 const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
         <title>Beat Shop</title>
+        {/* <link rel="icon" type="image/svg+xml" href='../public/favicon.ico'/> */}
+        <link rel="icon" type="image/x-icon" href=".../public/favicon.ico" />
+
       </Head>
       <header>
         <Navbar />
